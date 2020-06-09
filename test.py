26c2ddd019e6d928ea4b67c5aa9d0eafa9da957b
ori-kleinhauz <52367897+ori-kleinhauz@users.noreply.github.com>
2020-06-09 20:33:38 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import re
 from datetime import datetime
 
 
@@ -11,9 +13,17 @@ def get_list_of_desired_currencies():
     return
 
 
-
 page = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20110428&end=20200606')
 print(page)
 soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-(soup.prettify())
+# print(soup.prettify())
+# (soup.prettify())
+date_dirty = soup.find_all(class_="cmc-table__cell cmc-table__cell--sticky cmc-table__cell--left")
+test_dirty = str(date_dirty[1])
+print(test_dirty)
+end = test_dirty.index('</div></td>')
+test_clean = test_dirty [end-12:end]
+print(test_clean)
+date_time_obj = datetime.strptime(test_clean, '%b %d, %Y').date()
+print(date_time_obj)
+
