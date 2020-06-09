df99b2bdce225aedb6303d15a9f83683d3cebee3
Juvi <yuvalherm@gmail.com>
2020-06-09 17:15:00 +0300

    print HTML content

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+
+def update_database():
+    return
+
+
+def get_list_of_desired_currencies():
+    return
+
+
+
 page = requests.get('https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20110428&end=20200606')
 print(page)
 soup = BeautifulSoup(page.content, 'html.parser')
 print(soup.prettify())
+(soup.prettify())
